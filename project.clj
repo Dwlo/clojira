fa05441f2c780898059916062d3d4040825379fc
Dw <lo.dawid@gmail.com>
2016-01-29 22:52:56 +0100

    update of clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://clojira.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http            "1.0.1"]
                  [cheshire            "5.4.0"]
                  [doric               "0.9.0"]
